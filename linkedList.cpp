bc0748139cfd02043a7b62006f65b38a37679044
abdikitesa-tech <77311891+abdikitesa-tech@users.noreply.github.com>
2021-10-19 23:40:49 +0300

    Update linkedList.cpp

--- linkedList.cpp
+++ linkedList.cpp
@@ -15,10 +15,20 @@ struct node
 struct node* head;
 head = 0 ;
 
-int* p = new struct node; 
+int* p = new struct node;
+        int* t = new struct node; 
+        int* r = new struct node;  
 
-*p = 5;
+       p -> a = 1;
+       t = p;
+       r = p;
+
+        p = new struct node;
+        p -> a = 2;
+        t -> next = p;
+        t = t-> next;
 // delete p; // deletes what p is pointing to
 
 // p = new int (10);
-
+cout << "enter data";
+cin >>
